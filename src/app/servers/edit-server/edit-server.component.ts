346c0d956e010e1b0c5ba1263c731d144d9d051e
Soarabh Nain <soarabhnain@gmail.com>
2018-12-10 22:10:00 +0530

    routing till 134

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,13 +12,21 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit=false;
 
   constructor(private serversService: ServersService,
     private route:ActivatedRoute) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.route.queryParams
+    .subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    )
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ServersService } from '../servers.service';
+
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,24 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route:ActivatedRoute,
+    private router:Router ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    console.log(this.route.snapshot.params['id']);
+    const id= +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params
+    .subscribe(
+      (params:Params) => {
+        this.server= this.serversService.getServer(+params['id']);
+      }
+    );
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'],{relativeTo:this.route, queryParamsHandling:'preserve'})
   }
 
 }
